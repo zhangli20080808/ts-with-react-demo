cca317594202e970e30d3440af4e64a98e021471
zhangli20080808 <791492434@qq.com>
2022-09-25 11:57:36 +0800

    打包umd格式

--- rollup/rollup.umd.config.js
+++ rollup/rollup.umd.config.js
@@ -1,8 +1,30 @@
+import basicConfig from './rollup.config';
+import { terser } from 'rollup-plugin-terser';
+import replace from '@rollup/plugin-replace'; // 代码压缩器
 
-import typescript from "rollup-plugin-typescript2"
 const config = {
-input:'./scr/index.tsx',
-plugins
+  ...basicConfig,
+  output: [
+    {
+      name: 'designComponent',
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      exports: 'named', // 全局变量命名的方式
+      globals: { // 对应的第三方库
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        axios: 'Axios',
+      },
+      plugins: [terser()],
+    },
+  ],
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    ...basicConfig.plugins,
+  ],
+  external: ['react', 'react-dom', 'axios'],
 };
 
 export default config;
